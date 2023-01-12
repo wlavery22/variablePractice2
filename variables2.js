256614f51e6221c6f5d744ada16e30e69ac8777a
William Lavery <lavery_william@yahoo.com>
2023-01-12 11:30:56 -0500

    Add console.log statements

--- variables2.js
+++ variables2.js
@@ -1,3 +1,9 @@
 var address = "68 Birchall Dr."
 var Pomona = 47
 var employed = false
+address = "67 Birchall Dr."
+Pomona = 48
+employed = true
+console.log(address)
+console.log(Pomona)
+console.log(employed)
